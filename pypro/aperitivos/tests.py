015da1132d583a05e004a77f24511ff0176de777
Pichau <gustavosandrindattein123@gmail.com>
2022-02-03 16:09:37 -0300

    criado bloco de templates

--- pypro/aperitivos/tests.py
+++ pypro/aperitivos/tests.py
@@ -8,11 +8,14 @@ from pypro.django_assertions import assert_contains
 def resp(client):
     return client.get(reverse('aperitivos:video', args=('motivacao',)))
 
+
 def test_status_code(resp):
     assert resp.status_code == 200
 
+
 def test_titulo_video(resp):
-    assert_contains(resp, '<h1>Video Aperitivo: Motivação</h1>')
+    assert_contains(resp, '<h1 class="mt-4 mb-3">Video Aperitivo: Motivação</h1>')
+
 
 def test_conteudo_video(resp):
     assert_contains(resp, 'src="https://www.youtube.com/embed/2aYplgJrPDU"')
