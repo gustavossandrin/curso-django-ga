f7c0e727c9de91d145b925bbc1c561723cc2f41d
Pichau <gustavosandrindattein123@gmail.com>
2022-02-04 14:05:18 -0300

    video de acordo com o slug

--- pypro/aperitivos/views.py
+++ pypro/aperitivos/views.py
@@ -2,4 +2,9 @@ from django.shortcuts import render
 
 
 def video(request, slug):
-    return render(request, 'aperitivos/video.html')
+    videos = {
+        'motivacao': {'titulo': 'Video Aperitivo: Motivação', 'youtube_id': '2aYplgJrPDU'},
+        'instalacao-windows': {'titulo': 'instalação windows', 'youtube_id': 'ScmQ4I5Qr5s'},
+    }
+    video = videos[slug]
+    return render(request, 'aperitivos/video.html', context={'video': video})
