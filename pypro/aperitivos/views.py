e020242f78aabfd2893028c78e2974e19e17bd88
Pichau <gustavosandrindattein123@gmail.com>
2022-02-14 20:10:50 -0300

    final de testes de videos e criação de migração

--- pypro/aperitivos/views.py
+++ pypro/aperitivos/views.py
@@ -1,19 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from pypro.aperitivos.models import Video
 
 
-videos = [
-    Video(slug='motivacao', titulo='Video Aperitivo: Motivação', youtube_id='2aYplgJrPDU'),
-    Video(slug='instalacao-windows', titulo='instalação windows', youtube_id='ScmQ4I5Qr5s')
-]
 
-videos_dct = {v.slug: v for v in videos}
 
 
 def indice(request):
+    videos = Video.objects.order_by('creation').all()
     return render(request, 'aperitivos/indice.html', context={'videos': videos})
 
 
 def video(request, slug):
-    video = Video.objects.get(slug=slug)
+    video = get_object_or_404(Video, slug=slug)
     return render(request, 'aperitivos/video.html', context={'video': video})
