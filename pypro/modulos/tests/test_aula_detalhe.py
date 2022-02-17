8db7a40f328012097f0a876b0ff14a35563c15dd
Pichau <gustavosandrindattein123@gmail.com>
2022-02-17 13:37:59 -0300

    pagina de detalhe da aula

---
+++ pypro/modulos/tests/test_aula_detalhe.py
@@ -0,0 +1,30 @@
+import pytest
+from model_mommy import mommy
+
+from pypro.django_assertions import assert_contains
+from django.urls import reverse
+
+from pypro.modulos.models import Modulo, Aula
+
+
+@pytest.fixture
+def modulo(db):
+    return mommy.make(Modulo)
+
+
+@pytest.fixture
+def aula(modulo):
+    return mommy.make(Aula, modulo=modulo)
+
+
+@pytest.fixture
+def resp(client, aula):
+    resp = client.get(reverse('modulos:aula', kwargs={'slug': aula.slug}))
+    return resp
+
+
+def test_titulo(resp, aula: Aula):
+    assert_contains(resp, aula.titulo)
+
+
+
