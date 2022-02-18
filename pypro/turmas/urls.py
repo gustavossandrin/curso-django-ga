141eb495abdf81ee6f954846dcc1653339347c23
Pichau <gustavosandrindattein123@gmail.com>
2022-02-18 14:16:34 -0300

    campo manytomanyfield

--- pypro/turmas/urls.py
+++ pypro/turmas/urls.py
@@ -1,5 +1,4 @@
 """pypro URL Configuration
-
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
 Examples:
@@ -13,22 +12,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf import settings
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 
+from pypro.turmas import views
 
+app_name = 'turmas'
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('pypro.base.urls')),
-    path('aperitivos/', include('pypro.aperitivos.urls')),
-    path('modulos/', include('pypro.modulos.urls')),
-    path('turmas/', include('pypro.turmas.urls')),
+    path('', views.indice, name='indice'),
 ]
-
-if settings.DEBUG:
-    import debug_toolbar
-
-    urlpatterns.append(
-        path('__debug__/', include(debug_toolbar.urls))
-    )
