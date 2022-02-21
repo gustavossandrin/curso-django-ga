6c522c49b82ed84750b8e7ea1a1f9cc6114a553c
Pichau <gustavosandrindattein123@gmail.com>
2022-02-21 14:20:17 -0300

    dados de usuario no cabeçalho

--- pypro/base/tests/test_login.py
+++ pypro/base/tests/test_login.py
@@ -57,4 +57,9 @@ def test_link_de_login_indisponivel(resp_home_com_usuario_logado):
     assert_not_contains(resp_home_com_usuario_logado, reverse('login'))
 
 
+def test_botar_sair_disponivel(resp_home_com_usuario_logado):
+    assert_contains(resp_home_com_usuario_logado, 'Sair')
 
+
+def test_nome_usuario_logado_disponivel(resp_home_com_usuario_logado, usuario_logado):
+    assert_contains(resp_home_com_usuario_logado, usuario_logado.first_name)
