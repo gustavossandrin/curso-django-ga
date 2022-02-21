26679428a28c0d137fd2b338767dc03a370e67cc
Pichau <gustavosandrindattein123@gmail.com>
2022-02-21 17:06:25 -0300

    atualizado o bootstrap

--- pypro/modulos/tests/test_aba_de_modulos.py
+++ pypro/modulos/tests/test_aba_de_modulos.py
@@ -1,9 +1,8 @@
 import pytest
+from django.urls import reverse
 from model_mommy import mommy
 
 from pypro.django_assertions import assert_contains
-from django.urls import reverse
-
 from pypro.modulos.models import Modulo
 
 
@@ -25,4 +24,4 @@ def test_titulos_dos_modulos(resp, modulos):
 
 def test_link_dos_modulos(resp, modulos):
     for modulo in modulos:
-        assert_contains(resp, modulo.get_absolute_url)
+        assert_contains(resp, modulo.get_absolute_url())
